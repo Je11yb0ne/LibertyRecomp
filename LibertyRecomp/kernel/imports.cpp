5edfab17e0525ac00cc11af3662a3370a422392f
Je11yb0ne <james1599067665@gmail.com>
2026-06-17 15:00:09 +0800

    Fix Windows wrapper recursion at sub_82974530

--- LibertyRecomp/kernel/imports.cpp
+++ LibertyRecomp/kernel/imports.cpp
@@ -10909,10 +10909,11 @@ PPC_FUNC(sub_829745B0) {
 }
 
 extern "C" void sub_82974530(PPCContext& ctx, uint8_t* base);
+extern "C" void __imp__sub_82974530(PPCContext& ctx, uint8_t* base);
 PPC_FUNC(sub_82974530) {
     static int s_count = 0; ++s_count;
     LOGF_WARNING("[TRACE_EARLY] sub_82974530 ENTER #{}", s_count);
-    sub_82974530(ctx, base);
+    __imp__sub_82974530(ctx, base);
     LOGF_WARNING("[TRACE_EARLY] sub_82974530 EXIT #{} r3={}", s_count, ctx.r3.u32);
 }
 
